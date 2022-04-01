78ae8dd7efd3c4500577bea84c37321f7d0845b9
Username <avivfaraj4@gmail.com>
2022-04-01 12:42:54 -0400

    Added merge sort

--- Algorithms/Sorting/MergeSort.py
+++ Algorithms/Sorting/MergeSort.py
@@ -1,22 +1,63 @@
 import random
 import time
+from General import is_sorted
 
-def swap(arr: "List", a: int, b: int) -> None:
+
+def merge(arr: "List", start: int, middle: int, stop: int) -> None:
 	"""
-	Swap elements in an array
+	Merge two sorted lists
 
 	Input:
-	arr -> Array with numerical elements
-	a   -> index of the first element
-	b   -> index of the second element
+	arr 	-> Array with numerical elements
+	start   -> index of the first element
+	middle  -> index of the middle element
+	stop    -> index of the last element
 	"""
 
-	ind_ls = [i for i in range(0,len(arr))]
-	if a in ind_ls and b in ind_ls:
-		# Swap array's elements in indices a,b
-		arr[a], arr[b] = arr[b], arr[a]
-	else:
-		print("** Error ** One or more indices is invalid")
+	first_section = arr[start : middle + 1]
+	second_section = arr[middle + 1: stop + 1]
+
+	i,j = 0,0
+
+	for k in range(start, stop+1):
+		if i >= len(first_section):
+			arr[k] = second_section[j]
+			j += 1
+		elif j >= len(second_section):
+			arr[k] = first_section[i]
+			i += 1
+		elif second_section[j] > first_section[i]:
+			arr[k] = first_section[i]
+			i += 1
+		else:
+			arr[k] = second_section[j]
+			j += 1
+
+def merge_sort(arr: "List", start: int, stop: int) -> None:
+	"""
+	Implementation of merge sort
+
+	Input:
+	arr 	-> Array with numerical elements
+	start   -> index of the first element
+	stop    -> index of the last element
+	"""
+
+	# Terminate recursion
+	if start >= stop:
+		return
+
+	# Avoid overflow
+	middle = int(start + ((stop - start) / 2))
+
+	# Recursive iterations
+	# Left
+	merge_sort(arr, start, middle)
+	# Right
+	merge_sort(arr, middle+1, stop)
+
+	# Merge elements 
+	merge(arr, start, middle, stop)
 
 
 def test(num: int) -> None:
@@ -45,11 +86,10 @@ def test(num: int) -> None:
 	start = time.time()
 
 	# Sort
-	quick_sort(arr, 0, len(arr)-1)
+	merge_sort(arr, 0, len(arr)-1)
 
 	# Measure time of sorting
 	stop = time.time()
-
 	if is_sorted(arr):
 		# Print sorted array
 		print(f"Sorted Array: {arr}")
