da2f020df734b396b073317bf54b2b08ac314e01
avivfaraj <avivfaraj4@gmail.com>
2022-05-21 09:54:50 -0400

    Fixed flake8 errors for searching algorithms

--- Algorithms/Searching/BinarySearch.py
+++ Algorithms/Searching/BinarySearch.py
@@ -1,47 +1,50 @@
 from Test import test_sorted
 from typing import Optional, List
 
+
 def binary_search(arr: List[int], item: int) -> Optional[int]:
-	
-	start, stop = 0, len(arr) - 1
+    start, stop = 0, len(arr) - 1
 
-	while start <= stop:
+    while start <= stop:
 
-		# Avoid overflow
-		middle = int(start + (stop - start) / 2)
+        # Avoid overflow
+        middle = int(start + (stop - start) / 2)
 
-		if item == arr[middle]:
-			return middle
+        if item == arr[middle]:
+            return middle
 
-		if item < arr[middle]:
-			stop = middle - 1
+        if item < arr[middle]:
+            stop = middle - 1
 
-		else:
-			start = middle + 1
+        else:
+            start = middle + 1
 
-	return None
+    return None
 
-	
 
 def binary_recursive(arr: List[int], item: int) -> Optional[int]:
-	return recursive_iteration(arr, item, 0, len(arr) - 1)
+    return recursive_iteration(arr, item, 0, len(arr) - 1)
 
 
-def recursive_iteration(arr: List[int], item: int, start: int, stop: int) -> Optional[int]:
+def recursive_iteration(arr: List[int],
+                        item: int,
+                        start: int,
+                        stop: int) -> Optional[int]:
 
-	if start > stop:
-		return
+    if start > stop:
+        return
 
-	middle = int(start + (stop - start) / 2)
+    middle = int(start + (stop - start) / 2)
 
-	if item == arr[middle]:
-		return middle
+    if item == arr[middle]:
+        return middle
 
-	if item < arr[middle]:
-		return recursive_iteration(arr, item, start, middle - 1)
+    if item < arr[middle]:
+        return recursive_iteration(arr, item, start, middle - 1)
 
-	return recursive_iteration(arr, item, middle + 1, stop)
+    return recursive_iteration(arr, item, middle + 1, stop)
+
 
 if __name__ == "__main__":
-	test_sorted(binary_search)
-	test_sorted(binary_recursive)
+    test_sorted(binary_search)
+    test_sorted(binary_recursive)
--- Algorithms/Searching/LinearSearch.py
+++ Algorithms/Searching/LinearSearch.py
@@ -1,53 +1,58 @@
 from Test import test_generators, test
 from typing import List, Optional
+
+
 def linear_search(arr: List[int], item: int) -> Optional[int]:
 
-	# None if item not in arr
-	if not item in arr:
-		return None
+    # None if item not in arr
+    if item not in arr:
+        return None
 
-	i = 0
-	while i < len(arr):
-		if arr[i] == item:
-			return i
-		i += 1
+    i = 0
+    while i < len(arr):
+        if arr[i] == item:
+            return i
+        i += 1
 
 
 def linear_search_gen(arr: List[int], item: int) -> Optional[int]:
 
-	i = 0
-	while i < len(arr):
-		if arr[i] == item:
-			yield i
-		i += 1
+    i = 0
+    while i < len(arr):
+        if arr[i] == item:
+            yield i
+        i += 1
 
-	return None
+    return None
+
 
 def linear_recursive(arr: List[int], item: int) -> Optional[int]:
-	return recursive_iteration(arr, item, 0, len(arr) - 1)
+    return recursive_iteration(arr, item, 0, len(arr) - 1)
+
 
 def linear_recursive_gen(arr: List[int], item: int) -> int:
-	i = -1
-	while i < len(arr):
-		i = recursive_iteration(arr, item, i + 1, len(arr) - 1)
-		yield i
-
-def recursive_iteration(arr: List[int], item: int, start: int, stop: int) -> Optional[int]:
-
-	if start > stop:
-		return None
-
-	if item == arr[start]:
-		return start
-
-	return recursive_iteration(arr, item, start + 1, stop)
+    i = -1
+    while i < len(arr):
+        i = recursive_iteration(arr, item, i + 1, len(arr) - 1)
+        yield i
 
 
+def recursive_iteration(arr: List[int],
+                        item: int,
+                        start: int,
+                        stop: int) -> Optional[int]:
+
+    if start > stop:
+        return None
+
+    if item == arr[start]:
+        return start
+
+    return recursive_iteration(arr, item, start + 1, stop)
 
 
 if __name__ == "__main__":
-	test(linear_recursive)
-	test(linear_search)
-	test_generators(linear_recursive_gen)
-	test_generators(linear_search_gen)
-
+    test(linear_recursive)
+    test(linear_search)
+    test_generators(linear_recursive_gen)
+    test_generators(linear_search_gen)
