41a5ddc6511d8e8d91c5e568dd3e5ac843f4c8b9
avivfaraj <avivfaraj4@gmail.com>
2022-05-21 09:54:18 -0400

    Fixed flake8 errors for sorting algorithms

--- Algorithms/Sorting/InsertionSort.py
+++ Algorithms/Sorting/InsertionSort.py
@@ -1,22 +1,24 @@
-import random
-import time
-from General import swap, is_sorted, test
+from typing import List
+from General import swap
+from General import test
 
-def insertion_sort(arr: "List") -> None:
-	"""
-	Function that implements Insertion Sort O(n^2)
 
-	Input:
-	arr -> array to be sorted
-	"""
-	size = len(arr)
+def insertion_sort(arr: List) -> None:
+    """
+    Function that implements Insertion Sort O(n^2)
 
-	for i in range(1, size):
+    Input:
+    arr -> array to be sorted
+    """
+    size = len(arr)
 
-		for j in range(i,0, -1):
+    for i in range(1, size):
 
-			if arr[j-1] > arr[j]:
-				swap(arr, j-1, j)				
+        for j in range(i, 0, -1):
+
+            if arr[j-1] > arr[j]:
+                swap(arr, j-1, j)
+
 
 if __name__ == "__main__":
-	test(1000, insertion_sort)
+    test(1000, insertion_sort)
--- Algorithms/Sorting/RadixSort.py
+++ Algorithms/Sorting/RadixSort.py
@@ -1,33 +1,34 @@
-from General import swap, is_sorted, test
+from General import is_sorted
 from typing import List
 import math
 from collections import defaultdict
 
+
 def radix_sort(arr: List[int]) -> None:
-	"""
-	Implementation of Radix Sort
+    """
+    Implementation of Radix Sort
 
-	Input:
-	arr -> array to be sorted
-	"""
-	num_digits = math.floor(math.log10(max(arr))) + 1
+    Input:
+    arr -> array to be sorted
+    """
+    num_digits = math.floor(math.log10(max(arr))) + 1
 
-	for i in range(0, num_digits):
-		dict_ = defaultdict(list)
-		for j in arr:
-			dig = j // (10 ** (i)) % 10
-			dict_[dig].append(j)
-		
-		arr = []
-		for k in range(0,10):
-			arr.extend(dict_[k])
- 
-		del dict_
+    for i in range(0, num_digits):
+        dict_ = defaultdict(list)
+        for j in arr:
+            dig = j // (10 ** (i)) % 10
+            dict_[dig].append(j)
 
-	return arr
+        arr = []
+        for k in range(0, 10):
+            arr.extend(dict_[k])
+
+        del dict_
+
+    return arr
+
 
 if __name__ == "__main__":
-
-	arr =[1234, 4080, 200, 5010, 203, 40002]
-	arr = radix_sort(arr)
-	print(is_sorted(arr))
+    arr = [1234, 4080, 200, 5010, 203, 40002]
+    arr = radix_sort(arr)
+    print(is_sorted(arr))
