31fbe3c5773eebe2f660cad2dfd7aa51ab553a57
avivfaraj <avivfaraj4@gmail.com>
2022-07-19 23:13:00 -0500

    Added test for rolling hash function

--- Algorithms/General/Rolling_Hash.py
+++ Algorithms/General/Rolling_Hash.py
@@ -47,38 +47,3 @@ def rol_hash(text: str,
         hash_ = ((sub * 256) % prime + ord(text[-1])) % prime
 
     return hash_
-
-
-def test():
-    # Create a new instance
-    prime_inst = Prime()
-
-    # Store prime number
-    prime_num = prime_inst.rand
-    print("Current Prime Number:", prime_num)
-
-    # Hashing "This" (base 256)
-    # T -- 84
-    # h -- 104
-    # i -- 105
-    # s -- 115
-    hash_ = rol_hash("This", prime_num)
-    assert hash_ == (84 * 256 ** 3 +
-                     104 * 256 ** 2 +
-                     105 * 256 + 115) % prime_num, "Something is wrong"
-
-    # Checking the rolling hash --> From "This" to "his "
-    # h -- 104
-    # i -- 105
-    # s -- 115
-    # whitespace - 32
-    hash_ = rol_hash("his ", prime_num, subtract="T", last_hash=hash_)
-    assert hash_ == (104 * 256 ** 3 +
-                     105 * 256 ** 2 +
-                     115 * 256 + 32) % prime_num
-
-    print("Rolling Hash works fine!")
-
-
-if __name__ == "__main__":
-    test()
---
+++ Tests/test_algorithms/test_rolling_hash.py
@@ -0,0 +1,45 @@
+from Algorithms.General.Rolling_Hash import rol_hash
+from Algorithms.General.Prime import Prime
+
+import pytest
+
+@pytest.mark.parametrize("sample_str, n",
+                         [("This is just a test to check that rolling hash matches regular hash", 4),
+                          ("Second test with different window size", 10)])
+def test_rolling_hash(sample_str: str, n: int) -> None:
+    # Create a new instance
+    prime_inst = Prime()
+
+    # Store prime number
+    prime_num = prime_inst.rand
+
+    sample_str = "This is just a test to check that rolling hash matches regular hash"
+
+    # Window size
+    n = 4 
+
+    # Loop over windows with size 'n'
+    for i in range(len(sample_str) - n):
+
+        # Substring - window
+        sub_str = sample_str[i: i + n]
+
+        # Regular formula to calculate hash
+        # Less efficient because of the number of operations
+        # required such as multiplication / division ....
+        reg_hash = sum(ord(sub_str[j]) * 256 ** (n - j - 1) for j in range(n)) % prime_num
+
+        hash_ = -1
+
+        # First iteration - no previous hash
+        if i == 0:
+            hash_ = rol_hash(sub_str, prime_num)
+        
+        # Other iterations - subtract first char contribution
+        # and add the new char's contribution to the overall hash
+        else:
+            hash_ = rol_hash(sub_str, prime_num, sample_str[i - 1], hash_)
+
+        # Ensure both hashes are the same!
+        assert hash_ == reg_hash, "Something is wrong"
+
